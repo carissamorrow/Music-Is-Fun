63eddfd22721127efa021de5548675327bc7585b
carissa morrow <carissamorrow@hotmail.com>
2018-11-08 12:26:23 -0700

    cloned

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -8,28 +8,44 @@ function drawSongs(results) {
   console.log(results)
   //YOUR CODING STARTS HERE
 
+  let template = ''
+  for (let i = 0; i < results.length; i++) {
+    let song = results[i];
+    template += `
+
+      <div class="col-6">
+      <div row="">
+      <img src= "${song.albumArt}"
+          <p class= "${song.title}></p>
+          <p class= "${song.artist}></p>
+          <p class= "${song.collection}></p>
+          <p class= "${song.price}></p>
+          <p class= "${song.preview}></p> 
+        </div>
+      </div>
+      `
 
 
-}
-
-
-//PUBLIC
-class ItunesController {
-  //DO NOT MODIFY THIS METHOD
-  getMusic(e) {
-    e.preventDefault();
-    var artist = e.target.artist.value;
-    //changes the button to loading while songs load
-    $('#get-music-button').text('LOADING....');
-    itunesService.getMusicByArtist(artist).then(results => {
-      drawSongs(results)
-      //changes button back to GET MUSIC once songs are loaded
-      $('#get-music-button').text('GET MUSIC');
-    })
   }
 
 
-}
+  //PUBLIC
+  class ItunesController {
+    //DO NOT MODIFY THIS METHOD
+    getMusic(e) {
+      e.preventDefault();
+      var artist = e.target.artist.value;
+      //changes the button to loading while songs load
+      $('#get-music-button').text('LOADING....');
+      itunesService.getMusicByArtist(artist).then(results => {
+        drawSongs(results)
+        //changes button back to GET MUSIC once songs are loaded
+        $('#get-music-button').text('GET MUSIC');
+      })
+    }
 
 
-export default ItunesController
+  }
+
+
+  export default ItunesController
