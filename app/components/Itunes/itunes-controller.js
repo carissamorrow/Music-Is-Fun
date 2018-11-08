6bcf5b95ff19f9d2a667d81fa7e4377297cd492c
carissa morrow <carissamorrow@hotmail.com>
2018-11-08 12:31:00 -0700

    htmlfix

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -25,27 +25,29 @@ function drawSongs(results) {
       </div>
       `
 
+    document.getElementById('song-list').innerHTML = template
+  }
 
+}
+
+
+//PUBLIC
+class ItunesController {
+  //DO NOT MODIFY THIS METHOD
+  getMusic(e) {
+    e.preventDefault();
+    var artist = e.target.artist.value;
+    //changes the button to loading while songs load
+    $('#get-music-button').text('LOADING....');
+    itunesService.getMusicByArtist(artist).then(results => {
+      drawSongs(results)
+      //changes button back to GET MUSIC once songs are loaded
+      $('#get-music-button').text('GET MUSIC');
+    })
   }
 
 
-  //PUBLIC
-  class ItunesController {
-    //DO NOT MODIFY THIS METHOD
-    getMusic(e) {
-      e.preventDefault();
-      var artist = e.target.artist.value;
-      //changes the button to loading while songs load
-      $('#get-music-button').text('LOADING....');
-      itunesService.getMusicByArtist(artist).then(results => {
-        drawSongs(results)
-        //changes button back to GET MUSIC once songs are loaded
-        $('#get-music-button').text('GET MUSIC');
-      })
-    }
+}
 
 
-  }
-
-
-  export default ItunesController
+export default ItunesController
